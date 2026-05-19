b9d94404812da9517f11a7caef4e9efdcd225301
balagtaschristian65-svg <balagtaschristian65@gmail.com>
2026-05-19 15:16:55 +0800

    Add checkout config

---
+++ config.js
@@ -0,0 +1,6 @@
+window.DISPUTEDECK_CONFIG = {
+  selfServeCheckoutUrl: "https://balagtas6.gumroad.com/l/disputedeck-pro",
+  serviceCheckoutUrl: "https://balagtas6.gumroad.com/l/chargeback-packet-setup",
+  gumroadProductPermalink: "disputedeck-pro",
+  licenseVerificationEndpoint: "/.netlify/functions/verify-license"
+};
